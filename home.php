338a08b6a8864551719df867fb4b457bd9c3228f
Alan Fu <afu7568@stacmail.net>
2020-03-09 19:11:08 +1300

    n
    n

---
+++ home.php
@@ -0,0 +1,3 @@
+
+<!-- The Home Page. This is the default page displayed on the website. Shows the website's branding,
+ slogan, and gives links to the login and sign up page.-->
---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<!--Language is english-->
+<html lang='en'>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap' rel='stylesheet'>
+    <link rel='stylesheet' href='invictus.css'>
+
+    <!--Title of the website-->
+    <title>Invictus</title>
+  </head>
+  <body>
+    <!--Container fluid so the container is full-width instead of fixed-width-->
+    <div class='container-fluid'>
+      <div class='row'>
+        <!-- The websites logo, name, and slogan -->
+        <h3>Locate, Secure, <div  class='no'>Eliminate</div></h3>
+        <br><br><br><br>
+        <div class='col-12 text-center'>
+          <img src='logo2.png' height='100%' width='250px'>
+        </div>
+        <div class='col-12 text-center'>
+          <h1>I n v i <div class="yes">c t u s</div></h1>
+        </div>
+        <!-- Links for the loginsignup page -->
+        <div class='mt-3 col-12 text-center'>
+          <a class='py-2 px-3 mr-5 button-style-2' href='index.php?page=loginsignup'>Kill</a>
+          <a class='py-2 px-3 ml-5 button-style-1' href='index.php?page=loginsignup'>Elderly</a>
+        </div>
+        <!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2894.4266892722085!2d172.60702811568828!3d-43.4934335874895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d318b9f4f4e040f%3A0xc0ed68281f6bda47!2sNorthlands%20Shopping%20Centre!5e0!3m2!1sen!2snz!4v1583277779154!5m2!1sen!2snz" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>-->
+        <br><br><br><br>
+        <?php include('map.html');?>
+      </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous'></script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' integrity='sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy' crossorigin='anonymous'></script>
+  </body>
+</html>
